7c9f3b980fa49d4f8cc2f2b9ed68786b161f8d49
sajia <sajiazafreen@u.boisestate.edu>
2020-02-17 02:01:08 -0700

    Update Group.php

--- group.php
+++ group.php
@@ -11,7 +11,7 @@ include 'header.php';
   		<div id = "main">
   			<div id="Group1" class="Group"><strong>Cancer Patients</strong></div>
             <div id="Group2" class="Group"><strong>Physical or Mental Disability</strong></div>
-            <div id="Group3" class="Group"><strong>Physical or Mental Disability</strong></div>
+            <div id="Group3" class="Group"><strong>Accidents or Traumatic Experience</strong></div>
   		</div>
   	</div>
   </body>

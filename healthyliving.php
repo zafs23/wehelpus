2cb8170e6519ab05f143381e4f98051243ab6a3b
zafs23 <42818305+zafs23@users.noreply.github.com>
2020-04-27 14:38:09 -0600

    Adding Links

--- healthyliving.php
+++ healthyliving.php
@@ -9,9 +9,9 @@ include 'header.php';
   <body >
   	<div id = "container">
   		<div id = "main">
-  			<div id="Group1" class="Group"><strong>Mental Health Support</strong></div>
-            <div id="Group2" class="Group"><strong>Trauma Support</strong></div>
-            <div id="Group3" class="Group"><strong>Disability Support</strong></div>
+  			<div id="Group1" class="Group"><a href="https://www.health.harvard.edu/blog/diet-and-depression-2018022213309"><strong>Diet and Depression</strong></a></div>
+        <div id="Group2" class="Group"><a href="https://www.mayoclinic.org/diseases-conditions/depression/in-depth/depression-and-exercise/art-20046495" ><strong>Depression and Exercise</strong></a></div>
+        <div id="Group3" class="Group"><a href="https://www.psychologytoday.com/us/tests/health/mental-health-assessment"><strong>Mental Health Assessment</strong></a></div>
   		</div>
   	</div>
   </body>

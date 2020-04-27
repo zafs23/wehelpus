34f0a4886529377c1d43d40b1615ce122864af7e
zafs23 <42818305+zafs23@users.noreply.github.com>
2020-04-27 14:49:43 -0600

    Update healthy living page

--- healthyliving.php
+++ healthyliving.php
@@ -5,12 +5,16 @@ include 'header.php';
 <html>
   <head>
       <link rel="stylesheet" type="text/css" href="healthyliving.css">
+      <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Permanent+Marker&display=swap" rel="stylesheet">
+      <link href="https://fonts.googleapis.com/css2?family=Indie+Flower&family=Merienda+One&family=Permanent+Marker&display=swap" rel="stylesheet">
   </head>
   <body >
   	<div id = "container">
   		<div id = "main">
+          <h1 id = "head" >Here are some Life Hacks</h1>
   			<div id="Group1" class="Group"><a href="https://www.health.harvard.edu/blog/diet-and-depression-2018022213309"><strong>Diet and Depression</strong></a></div>
         <div id="Group2" class="Group"><a href="https://www.mayoclinic.org/diseases-conditions/depression/in-depth/depression-and-exercise/art-20046495" ><strong>Depression and Exercise</strong></a></div>
+        <h1 id = "head">Take a mental health assessment test: </h1>
         <div id="Group3" class="Group"><a href="https://www.psychologytoday.com/us/tests/health/mental-health-assessment"><strong>Mental Health Assessment</strong></a></div>
   		</div>
   	</div>
